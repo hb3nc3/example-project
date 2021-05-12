6f753d79c462bec96770442b2ace232a79ff5aff
hb3nc3 <horvath.bence.96@hotmail.com>
2021-05-12 22:34:15 +0200

    add project files

---
+++ src/main/java/io/github/hb3nc3/Main.java
@@ -0,0 +1,23 @@
+package io.github.hb3nc3;
+
+import io.github.hb3nc3.model.TestModel;
+import java.util.*;
+
+public class Main {
+
+    public void printSomething() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        TestModel model = new TestModel();
+        List<TestModel> models = new ArrayList();
+        Map<Integer,TestModel> testMap = new HashMap();
+        testMap.put(12, new TestModel());
+        models.add(model);
+        model.x = 5;
+        int[] array = {11,12,13,14,15};
+        Integer[] arr2 = null;
+        models.get(0).incrementAndPrint();
+    }
+}
---
+++ src/main/java/io/github/hb3nc3/model/TestModel.java
@@ -0,0 +1,11 @@
+package io.github.hb3nc3.model;
+
+public class TestModel {
+    public int x;
+
+    public void incrementAndPrint() {
+        String exampleString = "Example String in TestModel";
+        x = x + 1;
+        System.out.println(exampleString);
+    }
+}
